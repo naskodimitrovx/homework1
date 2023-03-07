36c3c0cdd42a8205244c1b9888eff527328e2b34
naskodimitrovx <126067421+naskodimitrovx@users.noreply.github.com>
2023-03-07 21:02:51 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿namespace zadacha7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int time = int.Parse(Console.ReadLine());
+            string day = Console.ReadLine();
+      if (day == "Monday" || day == "Tuesday" || day == "Wednesday" || day == "Thursday" || day == "Friday" || day == "Satuday" )
+            {
+
+                if (time >= 10 && time <= 18)
+                {
+                    Console.WriteLine("open");
+                }
+                
+            }
+      else
+            {
+                Console.WriteLine("closed");
+            }
+
+
+            }
+
+
+
+
+
+
+        }
+    }
