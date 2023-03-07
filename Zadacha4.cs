b6f4a2c863b18932e84418a7674065299a7d0e41
naskodimitrovx <126067421+naskodimitrovx@users.noreply.github.com>
2023-03-07 19:44:49 +0200

    Add files via upload

---
+++ Zadacha4.cs
@@ -0,0 +1,34 @@
+﻿namespace ConsoleApp8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+          double age = double.Parse(Console.ReadLine());
+            string gender = Console.ReadLine();
+
+            switch (gender)
+            {
+                case "m":
+                    if (age < 16)
+                    {
+                        Console.WriteLine("Master");
+                    }
+                    else
+                        Console.WriteLine("Mr.");
+
+                    break;
+                case "f":
+                    if (age < 16)
+                    {
+                         Console.WriteLine("Miss");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Ms.");
+                    }
+                    break;
+            }
+        }
+    }
+}
